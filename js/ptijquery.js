720c7477cdce6c82b060493e4d938e4e7a8b89a5
Guignol-Int <49168575+Guignol-Int@users.noreply.github.com>
2019-05-13 21:09:33 -0400

    Add files via upload

---
+++ js/ptijquery.js
@@ -0,0 +1,82 @@
+$("#car1").mouseenter(function () {
+    $('#car1').css('transform', 'scale(1.2)');
+});
+$("#car1").mouseleave(function () {
+    $('#car1').css('transform', 'scale(1)');
+});
+$("#car2").mouseenter(function () {
+    $('#car2').css('transform', 'scale(1.2)');
+});
+$("#car2").mouseleave(function () {
+    $('#car2').css('transform', 'scale(1)');
+});
+$("#car3").mouseenter(function () {
+    $('#car3').css('transform', 'scale(1.2)');
+});
+$("#car3").mouseleave(function () {
+    $('#car3').css('transform', 'scale(1)');
+});
+$("#car4").mouseenter(function () {
+    $('#car4').css('transform', 'scale(1.2)');
+});
+$("#car4").mouseleave(function () {
+    $('#car4').css('transform', 'scale(1)');
+});
+$("#car5").mouseenter(function () {
+    $('#car5').css('transform', 'scale(1.2)');
+});
+$("#car5").mouseleave(function () {
+    $('#car5').css('transform', 'scale(1)');
+});
+$("#car6").mouseenter(function () {
+    $('#car6').css('transform', 'scale(1.2)');
+});
+$("#car6").mouseleave(function () {
+    $('#car6').css('transform', 'scale(1)');
+});
+$("#car7").mouseenter(function () {
+    $('#car7').css('transform', 'scale(1.2)');
+});
+$("#car7").mouseleave(function () {
+    $('#car7').css('transform', 'scale(1)');
+});
+$("#car8").mouseenter(function () {
+    $('#car8').css('transform', 'scale(1.2)');
+});
+$("#car8").mouseleave(function () {
+    $('#car8').css('transform', 'scale(1)');
+});
+$("#car9").mouseenter(function () {
+    $('#car9').css('transform', 'scale(1.2)');
+});
+$("#car9").mouseleave(function () {
+    $('#car9').css('transform', 'scale(1)');
+});
+
+
+
+
+
+
+
+
+
+
+// jQuery obligatoire pour 2 pages libre ci-dessous -------------------------------------------
+
+var tasser = 0;
+
+$("#ohoh").click(function () {
+    tasser -= 20;
+    $('#b18b').css('margin-left',tasser + "px");
+});
+$("#uhuh").click(function () {
+    $('#lol').html("VTEEEEEEEEC", "h1");
+});
+$("#boutonChangerImage").click(function () {
+    $('#bou').attr("src", "img/bou.png");
+});
+
+
+
+
